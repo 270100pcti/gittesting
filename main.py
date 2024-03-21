369bc6eeb8407030fdbcb43370561a71ecc785d6
270100pcti <270100@pcti.mobi>
2024-03-21 13:43:25 -0400

    added requests

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
-import urllib.request
+import requests
 
 pygame.init()
-pygame.display.set_mode((1000,1000))
+pygame.display.set_mode((1920,1080))
+requests.post("https://8430699b-dd4a-453c-a428-cf272b51e8b1-00-38p10de3sgv1h.worf.replit.dev/",headers={"type":"join"})
+requests.post("https://8430699b-dd4a-453c-a428-cf272b51e8b1-00-38p10de3sgv1h.worf.replit.dev/",headers={"type":"leave"})
 
-urllib.request.Request("")
+
+requests.post("https://8430699b-dd4a-453c-a428-cf272b51e8b1-00-38p10de3sgv1h.worf.replit.dev/",headers={"type":"update","playerId":"1"},data={"posx":"0","posy":"0","heading":"0"})
 
 input('ok')
